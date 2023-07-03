385a489ce3a50a2711583209269692ededec2d55
pkiop <pkiopb@gmail.com>
2023-07-03 10:25:22 +0900

    add header

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return(
+        <div>
+            <div>ham</div>
+            <div>New chat</div>
+            <div>plus</div>
+        </div>
+    )
+}
+
+export default Header;
