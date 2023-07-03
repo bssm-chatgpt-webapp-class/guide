2674ee97fc8c27766049d2f2019afbc2917a114a
pkiop <pkiopb@gmail.com>
2023-07-03 10:58:08 +0900

    icon refector

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,13 @@
+import { HambugIcon, PlusIcon } from "../../icons";
+
 const Header = () => {
-    return(
-        <div>
-            <div>ham</div>
-            <div>New chat</div>
-            <div>plus</div>
-            <h1>제목</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <HambugIcon />
+      <div>New chat</div>
+      <PlusIcon />
+    </div>
+  );
+};
 
 export default Header;
