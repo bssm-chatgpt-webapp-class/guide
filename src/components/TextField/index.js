3bb2402e1daaed58d669c4ede0f35bffc93227d8
pkiop <pkiopb@gmail.com>
2023-07-03 13:42:15 +0900

    send button

--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -1,11 +1,17 @@
+import { useRef } from "react";
 import { SendIcon } from "../../icons";
 import "./index.css";
 
-const TextField = () => {
+const TextField = ({ setQuestion }) => {
+  const inputRef = useRef();
   return (
     <div className="text-field">
-      <input></input>
-      <SendIcon />
+      <input ref={inputRef}></input>
+      <SendIcon
+        onClick={() => {
+          setQuestion(inputRef.current.value);
+        }}
+      />
     </div>
   );
 };
--- src/icons/Send.js
+++ src/icons/Send.js
@@ -1,6 +1,7 @@
-const SendIcon = () => {
+const SendIcon = (props) => {
   return (
     <svg
+      {...props}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 16 16"
       fill="none"
